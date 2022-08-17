e0cd6dfa9b7ac9e122394677da8c131ffaefa20e
Ani <59758115+aniecon@users.noreply.github.com>
2022-08-17 11:33:13 -0400

    Update late_command.sh

--- Virtual/Machine/ISO-maker/Files/For-Target/late_command.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/late_command.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # For explanations, see econ-ark-tools/Virtual/Machine/ISO-maker/create-unattended-iso script
 
-apt -y update 
-apt -y reinstall git 
-apt -y install apt-utils
+# apt -y update 
+# apt -y reinstall git 
+# apt -y install apt-utils
+sudo pacman -Syy
+sudo pacman -S git
+
+
 mkdir -p /usr/local/share/data/GitHub/econ-ark 
 [[ ! -e /usr/local/share/data/GitHub/econ-ark/econ-ark-tools ]] && git clone --depth 1 --branch master https://github.com/econ-ark/econ-ark-tools /usr/local/share/data/GitHub/econ-ark/econ-ark-tools 
 chmod -R a+rwx /usr/local/share/data/GitHub/econ-ark/econ-ark-tools/* /usr/local/share/data/GitHub/econ-ark/econ-ark-tools/.*[0-z]* 
@@ -12,7 +16,7 @@ ln -s /usr/local/share/data/GitHub/econ-ark/econ-ark-tools/Virtual/Machine/ISO-m
 rm -f /var/local/status/Size-To-Make-Is-* 
 touch /var/local/status/Size-To-Make-Is-$(echo MIN) 
 [[ "$(/cdrom/preseed/*.iso)" != '' ]] &>/dev/null && mkdir -p /installer && cp /cdrom/preseed/*.iso /installer 
-cat /etc/apt/sources.list | grep -v cdrom > /tmp/apt-sources_without_cdrom.list 
-mv /tmp/apt-sources_without_cdrom.list /etc/apt/sources.list
+#cat /etc/apt/sources.list | grep -v cdrom > /tmp/apt-sources_without_cdrom.list 
+#mv /tmp/apt-sources_without_cdrom.list /etc/apt/sources.list
 /bin/bash -c "/var/local/late_command_finish.sh 2>&1 |& tee /var/local/status/late_command_finish.log" 
 /bin/bash -c "/var/local/tools/start-with-log.sh"
